6b147033966b4885fb1b6dfe22d133c3c7a64a4c
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-11-01 23:15:54 +0530

    update myScript.js

--- static/js/myScript.js
+++ static/js/myScript.js
@@ -1,128 +1,59 @@
-var i = 0;
-var id;
-var pr = false;
-var ne = false;
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'f7fb7156f2msh3fa7c0f58691b6fp1b826djsn3d9b72dfa245',
+    'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
+  }
+};
 
-function galleryR() {
-    //var i=Math.floor((Math.random() * 9) + 1);
-    var t = 40;
-    var k = 1500 / t;
-    var p = 1 / k;
-    var c;
-
-    document.getElementById("gallery1").src = "/static/img/pizza1.png";
-    document.getElementById("gallery1").style.opacity = 1;
-    document.getElementById("gallery2").src = "/static/img/pizza2.png";
-    document.getElementById("gallery2").style.opacity = 0;
-
-    function timeout() {
-        if ((pr) || (ne)) {
-            c = k + 1;
-        }
-        if (c <= k) {
-            document.getElementById("gallery1").style.opacity = parseFloat(document.getElementById("gallery1").style.opacity) - p;
-            document.getElementById("gallery2").style.opacity = parseFloat(document.getElementById("gallery2").style.opacity) + p;
-            c++;
-        } else {
-            clearInterval(id);
-            if ((!pr) && (!ne)) {
-                if (i == 0) {
-                    i++;
-                }
-                if (i < 9) {
-                    i++;
-                } else
-                if (i == 9) {
-                    i = 1;
-                }
-                document.getElementById("gallery1").src = "/static/img/pizza" + i + ".png";
-                document.getElementById("gallery1").style.opacity = 1;
-                if (i < 9) {
-                    document.getElementById("gallery2").src = "/static/img/pizza" + (i + 1) + ".png";
-                    document.getElementById("gallery2").style.opacity = 0;
-                } else {
-                    document.getElementById("gallery2").src = "/static/img/pizza1.png";
-                    document.getElementById("gallery2").style.opacity = 0;
-                }
-            }
-            if (pr) {
-                pr = false;
-            } else
-            if (ne) {
-                ne = false;
-            }
-            c = 1;
-            setTimeout(function() {
-                id = setInterval(timeout, t);
-            }, 2500);
-        }
-    }
-
-    c = 1;
-    setTimeout(function() {
-        id = setInterval(timeout, t);
-    }, 2500);
+fetch('https://crypto-news-live3.p.rapidapi.com/news', options)
+  .then(function(response) {
+    response.text().then(function(data) {
+      const Result = JSON.parse(data);
+      var CryptoNewsarea = document.getElementById('CryptoNewsIn');
+      for(let i = 0 ; i < 10; i++){
+        CryptoNewsh = Object.values(Result)[i].title;
+        UrlNews = Object.values(Result)[i].url;
+        
+        var CryptoNews = document.createElement('a');
+        //console.log(CryptoNews)
+        CryptoNews.className = "CrptoNewsD";
+        CryptoNews.href = `${UrlNews}`
+        CryptoNews.innerHTML = `● ${CryptoNewsh}`
+        //CryptoNews.title = `${CryptoNewsh}`
+        
+        const Breaker = document.createElement('hr');
+        Breaker.className = "Breaker";
+        
+        CryptoNewsarea.appendChild(CryptoNews)
+        CryptoNewsarea.appendChild(Breaker)
+      }
+    })
+  })
+  .catch(err => console.error(err));
+  
+  
+  
+  //.then(response => response.json())
+  //.then(response => console.log(response.keys[0]))
+  
+function chk(i) {
+  if (i < 10){
+    i = "0" + i
+  }
+  return i;
+}
+  
+const main = () => {
+  const d = new Date();
+  let hrs = chk(d.getHours());
+  let mins = chk(d.getMinutes());
+  let secs = chk(d.getSeconds());
+  let time = "Time : " + hrs + ":" + mins + ":" + secs;
+  document.getElementById("t").innerHTML = time;
 }
 
-function prev() {
-    pr = true;
-    if (i == 0) {
-        i = 9;
-    } else
-    if (i == 1) {
-        i = 9;
-    } else {
-        i--;
-    }
-    document.getElementById("gallery1").src = "/static/img/pizza" + i + ".png";
-    document.getElementById("gallery1").style.opacity = 1;
-    if (i < 9) {
-        document.getElementById("gallery2").src = "/static/img/pizza" + (i + 1) + ".png";
-        document.getElementById("gallery2").style.opacity = 0;
-    } else {
-        document.getElementById("gallery2").src = "/static/img/pizza1.png";
-        document.getElementById("gallery2").style.opacity = 0;
-    }
-}
+setInterval(() =>{
+  main();
+},10)
 
-function next() {
-    ne = true;
-    if (i == 0) {
-        i = 2;
-    } else
-    if (i == 9) {
-        i = 1;
-    } else {
-        i++;
-    }
-    document.getElementById("gallery1").src = "/static/img/pizza" + i + ".png";
-    document.getElementById("gallery1").style.opacity = 1;
-    if (i < 9) {
-        document.getElementById("gallery2").src = "/static/img/pizza" + (i + 1) + ".png";
-        document.getElementById("gallery2").style.opacity = 0;
-    } else {
-        document.getElementById("gallery2").src = "/static/img/pizza1.png";
-        document.getElementById("gallery2").style.opacity = 0;
-    }
-}
-
-window.onload = galleryR;
-
-var myCenter = new google.maps.LatLng(45.253992, 9.989992);
-var marker;
-
-function initialize() {
-    var mapProp = {
-        center: new google.maps.LatLng(45.253992, 9.989992),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.ROAD
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapProp);
-
-    var marker = new google.maps.Marker({
-        position: myCenter
-    });
-
-    marker.setMap(map);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
